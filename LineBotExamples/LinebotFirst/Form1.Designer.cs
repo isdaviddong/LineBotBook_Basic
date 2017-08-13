26059efa3de037cb87deae5e4c346c653d647ebd
David@SurfacePro4 <isdaviddong@gmail.com>
2017-08-13 15:46:18 +0800

    貼圖與圖片

--- LineBotExamples/LinebotFirst/Form1.Designer.cs
+++ LineBotExamples/LinebotFirst/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -42,11 +44,35 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("PMingLiU", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(71, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(390, 76);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "發送貼圖";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("PMingLiU", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(71, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(390, 76);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "發送圖片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1159, 651);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- LineBotExamples/LinebotFirst/Form1.cs
+++ LineBotExamples/LinebotFirst/Form1.cs
@@ -12,6 +12,9 @@ namespace LinebotFirst
 {
     public partial class Form1 : Form
     {
+        const string token = ""; //換成你的token
+        const string AdminUserId = ""; //換成你的UserId
+
         public Form1()
         {
             InitializeComponent();
@@ -24,6 +27,25 @@ namespace LinebotFirst
             isRock.LineBot.Bot bot = new isRock.LineBot.Bot(token);
 
             bot.PushMessage(AdminUserId, "測試訊息");
+        }
+
+        //發送貼圖鈕
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot(token);
+            //發送貼圖
+            bot.PushMessage(AdminUserId, 1,2);
+        }
+
+        //發送圖片鈕
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot(token);
+
+            //指定圖片位置
+            Uri imgURL = new Uri("https://arock.blob.core.windows.net/blogdata201706/22-124357-ad3c87d6-b9cc-488a-8150-1c2fe642d237.png");
+            //發送圖片
+            bot.PushMessage(AdminUserId,imgURL);
         }
     }
 }
