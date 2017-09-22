927a4273cc08d1f8c910982412dc643dbeb20efd
David@SurfacePro4 <isdaviddong@gmail.com>
2017-09-22 14:30:37 +0800

    first controller

--- LineBotExamples/WebHook_First/Controllers/LineBotWebHookController.cs
+++ LineBotExamples/WebHook_First/Controllers/LineBotWebHookController.cs
@@ -7,13 +7,14 @@ using System.Web.Http;
 
 namespace WebHook_First.Controllers
 {
+  
     public class LineBotWebHookController : ApiController
     {
         [HttpPost]
         public IHttpActionResult POST()
         {
-            string ChannelAccessToken = "請用你自己的Channel Access Token";
-
+            string ChannelAccessToken = "Omj1rwtDh+eP0wb+KbcbLF0+tpdrlDz7G+cfXXvmrf4W8gBz7x9VNgyApN62ZY31muFeXll6qFCKrbsJyfYxgt6qqxoKZ7ygxdWIHFkIudhxrzj46W2tXjY+djMfTVI1RrYfNptLOjQA7ARXs2JRHAdB04t89/1O/w1cDnyilFU=";
+    
             try
             {
                 //取得 http Post RawData(should be JSON)
