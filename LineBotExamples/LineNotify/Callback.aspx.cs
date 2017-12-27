7160e8933986798fdbc444fa04d0f588ca248986
David@SurfacePro4 <isdaviddong@gmail.com>
2017-12-27 17:06:33 +0800

    加上LineNotify範例

---
+++ LineBotExamples/LineNotify/Callback.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LineNotify
+{
+    public partial class Callback : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //取得返回的code
+            var code = Request.QueryString["code"];
+            if (code == null)
+            {
+                Response.Write("沒有正確回應code");
+                Response.End();
+            }
+            //顯示，測試用
+            Response.Write("<br/> code : " + code);
+            //從Code取回toke
+            var token = isRock.LineNotify.Utility.GetTokenFromCode(code,
+                "FkAzcte6WjGR1s2PhqEP8b",  //TODO:請更正為你自己的 client_id
+                "Y1dRkTf8LIKfbNZlcH6Op6lQLZovz2daEoa1Rd0zfdx", //TODO:請更正為你自己的 client_secret
+                "http://localhost:49100/Callback.aspx");
+            //顯示，測試用
+            //(注意這是範例，token不該用明碼傳遞，也不該出現在用戶端，你應該自行記錄在資料庫或ServerSite session中)
+            Response.Write("<br/> token : " + token.access_token);
+            //利用token順手發個測試訊息
+            isRock.LineNotify.Utility.SendNotify(token.access_token, "msg test - " + System.DateTime.Now.ToString());
+        }
+    }
+}
---
+++ LineBotExamples/LineNotify/SendNotify.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendNotify.aspx.cs" Inherits="LineNotify.SendNotify" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Token:<asp:TextBox ID="TextBoxToken" runat="server" Width="621px"></asp:TextBox>
+        <br />
+        <br />
+        Messang:<asp:TextBox ID="TextBoxMsg" runat="server" Height="123px" TextMode="MultiLine" Width="593px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Send" />
+    
+    </div>
+    </form>
+</body>
+</html>
